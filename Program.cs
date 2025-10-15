c4791eaeb20a1909d81319a35061a86b91d3d724
Ava Manning <362579849@GoTVDSB.ca>
2025-10-15 08:43:00 -0400

    Done

--- Program.cs
+++ Program.cs
@@ -9,142 +9,142 @@ namespace Topic_5_asignment
         {
 
 
-            //double venus, mars, jupiter, saturn, uranus, neptune;
-            //double earthWeight, planetWeight;
-            //string planet;
+            double venus, mars, jupiter, saturn, uranus, neptune;
+            double earthWeight, planetWeight;
+            string planet;
 
 
-            //venus = 0.78;
-            //mars = 0.39;
-            //jupiter = 2.65;
-            //saturn = 1.17;
-            //uranus = 1.05;
-            //neptune = 1.23;
+            venus = 0.78;
+            mars = 0.39;
+            jupiter = 2.65;
+            saturn = 1.17;
+            uranus = 1.05;
+            neptune = 1.23;
 
 
 
 
-            //Console.WriteLine("Please enter your current earth weight");
-            //double.TryParse(Console.ReadLine(), out earthWeight);
+            Console.WriteLine("Please enter your current earth weight");
+            double.TryParse(Console.ReadLine(), out earthWeight);
 
-            //Console.WriteLine("I have information for the following 6 planets");
-            //Console.WriteLine(" 1.Venus  2.Mars  3.Jupiter  4.Saturn  5.Uranus  6.Neptune");
-            //Console.WriteLine("Which planet are you visiting?");
-            //planet = Console.ReadLine();
+            Console.WriteLine("I have information for the following 6 planets");
+            Console.WriteLine(" 1.Venus  2.Mars  3.Jupiter  4.Saturn  5.Uranus  6.Neptune");
+            Console.WriteLine("Which planet are you visiting?");
+            planet = Console.ReadLine();
 
-            //if (planet.ToLower() == "venus" || planet == "1")
-            //{
-            //    planetWeight = earthWeight * venus;
-            //    Console.WriteLine("Your weight would be " + planetWeight + " pounds on Venus.");
+            if (planet.ToLower() == "venus" || planet == "1")
+            {
+                planetWeight = earthWeight * venus;
+                Console.WriteLine("Your weight would be " + planetWeight + " pounds on Venus.");
 
-            //}
+            }
 
-            //else if (planet.ToLower() == "mars" || planet == "2")
-            //{
-            //    planetWeight = earthWeight * mars;
-            //    Console.WriteLine("Your weight would be " + planetWeight + " pounds on mars.");
+            else if (planet.ToLower() == "mars" || planet == "2")
+            {
+                planetWeight = earthWeight * mars;
+                Console.WriteLine("Your weight would be " + planetWeight + " pounds on mars.");
 
-            //}
+            }
 
-            //else if (planet.ToLower() == "jupiter" || planet == "3")
-            //{
-            //    planetWeight = earthWeight * jupiter;
-            //    Console.WriteLine("Your weight would be " + planetWeight + " pounds on jupiter.");
+            else if (planet.ToLower() == "jupiter" || planet == "3")
+            {
+                planetWeight = earthWeight * jupiter;
+                Console.WriteLine("Your weight would be " + planetWeight + " pounds on jupiter.");
 
-            //}
+            }
 
-            //else if (planet.ToLower() == "staurn" || planet == "4")
-            //{
-            //    planetWeight = earthWeight * saturn;
-            //    Console.WriteLine("Your weight would be " + planetWeight + " pounds on saturn.");
+            else if (planet.ToLower() == "staurn" || planet == "4")
+            {
+                planetWeight = earthWeight * saturn;
+                Console.WriteLine("Your weight would be " + planetWeight + " pounds on saturn.");
 
-            //}
-            //else if (planet.ToLower() == "uranus" || planet == "5")
-            //{
-            //    planetWeight = earthWeight * uranus;
-            //    Console.WriteLine("Your weight would be " + planetWeight + " pounds on uranus.");
+            }
+            else if (planet.ToLower() == "uranus" || planet == "5")
+            {
+                planetWeight = earthWeight * uranus;
+                Console.WriteLine("Your weight would be " + planetWeight + " pounds on uranus.");
 
-            //}
+            }
 
-            //else if (planet.ToLower() == "neptune" || planet == "6")
-            //{
-            //    planetWeight = earthWeight * neptune;
-            //    Console.WriteLine("Your weight would be " + planetWeight + " pounds on neptune.");
+            else if (planet.ToLower() == "neptune" || planet == "6")
+            {
+                planetWeight = earthWeight * neptune;
+                Console.WriteLine("Your weight would be " + planetWeight + " pounds on neptune.");
 
-            //}
-            //else
-            //{
+            }
+            else
+            {
 
-            //    Console.WriteLine("Error");
+                Console.WriteLine("Error");
 
-            //}
-
-
-            //string correctAnswer, color;
-            //int legs, equation;
-            //int tempature;
-            //int score;
-
-            //score = 0;
-
-            //Console.WriteLine("Quiz time!");
-            //Console.WriteLine();
-            //Console.WriteLine("How many legs does a dog have?");
-            //int.TryParse(Console.ReadLine(), out legs);
-            //if (legs == 4)
-            //{
-            //    Console.WriteLine("You are correct!");
-            //    score = score + 1;
-            //}
-            //else
-            //{
-            //    Console.WriteLine("You are incorrect!");
-
-            //}
-            //Console.WriteLine();
-            //Console.WriteLine("What is 3*(2-1)?");
-            //int.TryParse(Console.ReadLine(), out equation);
-            //if (equation == 3)
-            //{
-            //    Console.WriteLine("You are correct!");
-            //    score = score + 1;
-            //}
-            //else
-            //{
-            //    Console.WriteLine("You are incorrect!");
-            //}
-
-            //Console.WriteLine();
-            //Console.WriteLine("Enter any tempature in degrees celsius at wich water is liquid");
-            //int.TryParse(Console.ReadLine(), out tempature);
-            //if (tempature >= 0 && tempature <= 100)
-            //{
-            //    Console.WriteLine("You are Correct!");
-            //    score = score + 1;
-            //}
-
-            //Console.WriteLine("What is the color of a stop sign?");
-            //Console.WriteLine("Your options are A.blue B.yellow C.red D.green");
-            //Console.WriteLine("Wich one is correct?");
-            //color = Console.ReadLine();
-
-            //correctAnswer = "c";
-
-            //if (color.ToLower() == "red" || color.ToLower() == correctAnswer)
-            //{
-            //    Console.WriteLine("You are correct!");
-            //    score = score + 1;
-            //}
-
-            //else
-            //{
-            //    Console.WriteLine("You are incorrect");
-            //}
-
-            //Console.WriteLine("Your final score is " + score);
+            }
 
 
-            double firstNumber, secondNumber, equation;
+            string correctAnswer, color;
+            int legs, equation;
+            int tempature;
+            int score;
+
+            score = 0;
+
+            Console.WriteLine("Quiz time!");
+            Console.WriteLine();
+            Console.WriteLine("How many legs does a dog have?");
+            int.TryParse(Console.ReadLine(), out legs);
+            if (legs == 4)
+            {
+                Console.WriteLine("You are correct!");
+                score = score + 1;
+            }
+            else
+            {
+                Console.WriteLine("You are incorrect!");
+
+            }
+            Console.WriteLine();
+            Console.WriteLine("What is 3*(2-1)?");
+            int.TryParse(Console.ReadLine(), out equation);
+            if (equation == 3)
+            {
+                Console.WriteLine("You are correct!");
+                score = score + 1;
+            }
+            else
+            {
+                Console.WriteLine("You are incorrect!");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Enter any tempature in degrees celsius at wich water is liquid");
+            int.TryParse(Console.ReadLine(), out tempature);
+            if (tempature >= 0 && tempature <= 100)
+            {
+                Console.WriteLine("You are Correct!");
+                score = score + 1;
+            }
+
+            Console.WriteLine("What is the color of a stop sign?");
+            Console.WriteLine("Your options are A.blue B.yellow C.red D.green");
+            Console.WriteLine("Wich one is correct?");
+            color = Console.ReadLine();
+
+            correctAnswer = "c";
+
+            if (color.ToLower() == "red" || color.ToLower() == correctAnswer)
+            {
+                Console.WriteLine("You are correct!");
+                score = score + 1;
+            }
+
+            else
+            {
+                Console.WriteLine("You are incorrect");
+            }
+
+            Console.WriteLine("Your final score is " + score);
+
+
+            double firstNumber, secondNumber,total;
             string sign;
 
             Console.WriteLine("What is the first number youd like to insert?");
@@ -154,10 +154,34 @@ namespace Topic_5_asignment
             Console.WriteLine("What is the second number you would like to insert?");
             Double.TryParse(Console.ReadLine(), out secondNumber);
 
-            equation = firstNumber + sign + secondNumber ;
+            if (sign == "+")
+            {
+                total = firstNumber + secondNumber;
+                Console.WriteLine(firstNumber + " + " + secondNumber + " = " + total);
 
+            }
 
-            
+            if (sign == "-")
+            {
+                total = firstNumber - secondNumber;
+                Console.WriteLine(firstNumber + " - " + secondNumber + " = " + total);
+
+            }
+
+            if (sign == "*")
+            {
+                total = firstNumber * secondNumber;
+                Console.WriteLine(firstNumber + " * " + secondNumber + " = " + total);
+
+            }
+
+            if (sign == "/")
+            {
+                total = firstNumber / secondNumber;
+                Console.WriteLine(firstNumber + " / " + secondNumber + " = " + total);
+
+            }
+
 
 
         }
